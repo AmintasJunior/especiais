b6a62ff7c49041129e640731e1a8ced5347a3a22
Amintas Junior <amintas.geru@gmail.com>
2024-04-10 12:17:00 -0300

    att do modal e filtros

--- exportarDados.js
+++ exportarDados.js
@@ -8,32 +8,32 @@ function exportToExcel() {
   XLSX.writeFile(wb, "Transferências Especiais - AC.xlsx")
 }
 
-function exportToPDF() {
-  const options = {
-    margin: 1,
-    filename: "documento.pdf",
-    image: { type: "png", quality: 1 },
-    html2canvas: { scale: 2 },
-    jsPDF: { unit: "in", format: "a4", orientation: "portrait" },
-    header: {
-      height: "1in",
-      contents: '<img src="./img/cabecalho.png" style="width: 100%;" />',
-    },
-    footer: {
-      height: "1in",
-      contents: '<img src="./img/rodape.png" style="width: 100%;" />',
-    },
-  }
+// function exportToPDF() {
+//   const options = {
+//     margin: 1,
+//     filename: "documento.pdf",
+//     image: { type: "png", quality: 1 },
+//     html2canvas: { scale: 2 },
+//     jsPDF: { unit: "in", format: "a4", orientation: "portrait" },
+//     header: {
+//       height: "1in",
+//       contents: '<img src="./img/cabecalho.png" style="width: 100%;" />',
+//     },
+//     footer: {
+//       height: "1in",
+//       contents: '<img src="./img/rodape.png" style="width: 100%;" />',
+//     },
+//   }
 
-  const content = document.getElementById("dadosTabela")
+//   const content = document.getElementById("dadosTabela")
 
-  html2pdf().from(content).set(options).save()
-}
+//   html2pdf().from(content).set(options).save()
+// }
 
-document
-  .getElementById("exportPDFButton")
-  .addEventListener("click", exportToPDF)
+// document
+//   .getElementById("exportPDFButton")
+//   .addEventListener("click", exportToPDF)
 
-document
-  .getElementById("exportPDFButton")
-  .addEventListener("click", exportToPDF)
+// document
+//   .getElementById("exportPDFButton")
+//   .addEventListener("click", exportToPDF)
